3c29d1bc2bdfdb6c1c3ef1b31211ceb037013cfd
LoicMonard <bobbycarotte@outlook.fr>
2016-10-26 16:13:44 +0200

    Add vue-resource package.json

--- src/OmegaWebApp/omega_front/src/main.js
+++ src/OmegaWebApp/omega_front/src/main.js
@@ -1,7 +1,11 @@
 import 'babel-polyfill'
 import Vue from 'vue'
+import Router from 'vue-router'
 import store from './vuex/store'
 import App from './components/App.vue'
+
+Vue.use(Router)
+Vue.use(require('vue-resource'))
 
 new Vue({
   el: '#app',
