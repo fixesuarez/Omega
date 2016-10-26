be8df9e758fc8a272262766b1f09938b1bb20fac
Fx Suarez <francoisxaviersuarez@gmail.com>
2016-10-26 13:53:23 +0200

    Revert "Revert "beginning of Oauth""
    This reverts commit b9244072324fa121ac6f32f315e7e5bc59d50d08.
    
    Conflicts:
    	src/Crawler/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache
    	src/Omega.Crawler.Tests/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache
    	src/Omega.DataManager.Tests/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache
    	src/OmegaWebApp/obj/Debug/netcoreapp1.0/.IncrementalCache
    	src/OmegaWebApp/obj/Debug/netcoreapp1.0/OmegaWebAppdotnet-compile.deps.json
    	src/OmegaWebApp/obj/Debug/netcoreapp1.0/dotnet-compile-csc.rsp
    	src/OmegaWebApp/obj/Debug/netcoreapp1.0/dotnet-compile.rsp

---
+++ src/Omega.DAL/User.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Omega.DAL
+{
+    public class User : TableEntity
+    {
+        public string Email
+        {
+            get { return RowKey; }
+            set { RowKey = value; }
+        }
+
+        public string FacebookId { get; set; }
+        public string SpotifyId { get; set; }
+        public string DeezerId { get; set; }
+
+        public string FacebookAccessToken { get; set; }
+
+        public string DeezerAccessToken { get; set; }
+        public string DeezerRefreshToken { get; set; }
+
+        public string SpotifyAccessToken { get; set; }
+        public string SpotifyRefreshToken { get; set; }
+
+        public User()
+        {
+        }
+    }
+}
---
+++ src/OmegaWebApp/Authentication/SpotifyAppBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+
+namespace OmegaWebApp.Authentication
+{
+    public static IApplicationBuilder UseSpotifyAuthentication( this IApplicationBuilder app, Action<SpotifyOptions> configuration )
+    {
+        SpotifyOptions options = new SpotifyOptions();
+        configuration( options );
+        app.UseSpotifyAuthentication( options );
+        return app;
+    }
+}
