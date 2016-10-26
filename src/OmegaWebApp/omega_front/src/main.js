c1ed8d9dda19c46e936d2e52d48c23db41ba1fd0
LoicMonard <bobbycarotte@outlook.fr>
2016-10-26 16:23:22 +0200

    Test

--- src/OmegaWebApp/omega_front/src/main.js
+++ src/OmegaWebApp/omega_front/src/main.js
@@ -1,7 +1,8 @@
 import 'babel-polyfill'
 import Vue from 'vue'
 import Router from 'vue-router'
-import store from './vuex/store'
+import Login from './components/Login.vue'
+import Login from './components/Login.vue'
 import Login from './components/Login.vue'
 
 Vue.use(Router)
