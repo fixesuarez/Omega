fe52319e3c4dc2adaa1b879a79138be7e71c4965
LoicMonard <bobbycarotte@outlook.fr>
2016-11-14 09:26:45 +0100

    Setting up the way to call components

--- src/OmegaWebApp/omega_front/src/main.js
+++ src/OmegaWebApp/omega_front/src/main.js
@@ -5,6 +5,9 @@ import store from './vuex/store'
 import App from './App.vue'
 import controlPanel from './components/controlPanel.vue'
 import Login from './components/Login.vue'
+import playlists from './components/playlists.vue'
+import events from './components/events.vue'
+import groups from './components/groups.vue'
 
 Vue.use(Router)
 Vue.use(require('vue-resource'))
@@ -12,8 +15,10 @@ Vue.use(require('vue-resource'))
 const router = new Router({
   mode: 'history',
   routes: [
-    { path: '', component: controlPanel },
-    { path: '/login', component: Login }
+    { path: '', component: playlists },
+    { path: '/playlists', component: playlists },
+    { path: '/groups', component: groups },
+    { path: '/events', component: events }
   ]
 })
 
