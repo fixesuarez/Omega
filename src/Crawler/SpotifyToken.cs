de8ba9879cce7d461c56f5b64dff6e30889946d4
Thibault Cam <thibault.cam1@yahoo.fr>
2016-11-18 12:32:58 +0100

    Add CodeCakeBuilder

--- src/Crawler/SpotifyToken.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Omega.Crawler
-{
-    public class SpotifyToken
-    {
-        public string access_token { get; set; }
-        public string token_type { get; set; }
-        public int expires_in { get; set; }
-    }
-}
