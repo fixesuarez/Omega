6149b751a3c4ab699d9e8ea68312098ea6674ff7
Fx Suarez <suarez@intechinfo.fr>
2016-11-28 11:03:47 +0100

    small changes

--- src/OmegaWebApp/wwwroot/_references.js
+++ src/OmegaWebApp/wwwroot/_references.js
@@ -5,6 +5,7 @@
 /// <reference path="../omega_front/src/services/AuthService.js" />
 /// <reference path="../omega_front/src/services/deezerapiservice.js" />
 /// <reference path="../omega_front/src/services/FacebookApiService.js" />
+/// <reference path="../omega_front/src/services/playlistapiservice.js" />
 /// <reference path="../omega_front/src/services/spotifyapiservice.js" />
 /// <reference path="../omega_front/src/vuex/actions.js" />
 /// <reference path="../omega_front/src/vuex/getters.js" />
