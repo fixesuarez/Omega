5ade882ecd0844d62eb7dbb10b832da38e3c6d78
Thibault Cam <thibault.cam1@yahoo.fr>
2016-12-16 15:22:32 +0100

    FacebookCrawler works

--- src/Omega.DAL/EventGroupGateway.cs
+++ src/Omega.DAL/EventGroupGateway.cs
@@ -22,14 +22,14 @@ namespace Omega.DAL
             _storageAccount = CloudStorageAccount.Parse(connectionString);
             _tableClient = _storageAccount.CreateCloudTableClient();
             _tableEventGroup = _tableClient.GetTableReference("EventGroup");
-            _tableEventGroup.CreateIfNotExistsAsync();
+            _tableEventGroup.CreateIfNotExistsAsync().Wait();
 
             _queueClient = _storageAccount.CreateCloudQueueClient();
-            _normalQueue = _queueClient.GetQueueReference("normalQueue");
-            _normalQueue.CreateIfNotExistsAsync();
+            _normalQueue = _queueClient.GetQueueReference("normalqueue");
+            _normalQueue.CreateIfNotExistsAsync().Wait();
             
-            _priorityQueue = _queueClient.GetQueueReference("priorityQueue");
-            _priorityQueue.CreateIfNotExistsAsync();
+            _priorityQueue = _queueClient.GetQueueReference("priorityqueue");
+            _priorityQueue.CreateIfNotExistsAsync().Wait();
         }
 
         public async Task InsertEventGroup(string eventId, List<User> users, string type, string cover)
@@ -48,9 +48,32 @@ namespace Omega.DAL
                     eventGroup.Cover = cover;
                     batchOperation.Insert( eventGroup );
                 }
+                else
+                {
+                    await UpdateEventGroup(eventId, user, type, cover);
+                }
             }
             if( batchOperation.Count != 0)
                 await _tableEventGroup.ExecuteBatchAsync(batchOperation);
+        }
+
+        public async Task UpdateEventGroup(string eventId, User user, string type, string cover)
+        {
+            TableOperation retrieveOperation = TableOperation.Retrieve<EventGroup>(eventId, user.Email);
+            TableResult retrievedResult = await _tableEventGroup.ExecuteAsync(retrieveOperation);
+            EventGroup updateEntity = (EventGroup)retrievedResult.Result;
+
+            if (updateEntity != null)
+            {
+                EventGroup track = new EventGroup(eventId, user.Email);
+                updateEntity.UserId = user.FacebookId;
+                updateEntity.Type = type;
+                updateEntity.Cover = cover;
+
+                TableOperation updateOperation = TableOperation.Replace(updateEntity);
+
+                await _tableEventGroup.ExecuteAsync(updateOperation);
+            }
         }
 
         public async Task<EventGroup> RetrieveGroupEvent( string eventGroupId, string email )
@@ -72,9 +95,31 @@ namespace Omega.DAL
             await _priorityQueue.AddMessageAsync(message);
         }
 
-        public async Task<TableContinuationToken> PriorityContinuationToken()
+        public async Task<TableQuerySegment<EventGroup>> TableQueryResult()
         {
-            throw new NotImplementedException();
+            TableContinuationToken continuationToken = null;
+            TableQuery<EventGroup> tableQuery = new TableQuery<EventGroup>();
+            TableQuerySegment<EventGroup> tableQueryResult;
+            return tableQueryResult = await _tableEventGroup.ExecuteQuerySegmentedAsync(tableQuery, continuationToken);
+        }
+
+        public async Task DeleteMessagePriorityQueue(CloudQueueMessage message)
+        {
+            await _priorityQueue.DeleteMessageAsync(message);
+        }
+
+        public async Task DeleteMessageNormalQueue(CloudQueueMessage message)
+        {
+            await _normalQueue.DeleteMessageAsync(message);
+        }
+
+        public async Task<CloudQueueMessage> GetMessagePriorityQueue()
+        {
+            return await _priorityQueue.GetMessageAsync();
+        }
+        public async Task<CloudQueueMessage> GetMessageNormalQueue()
+        {
+            return await _normalQueue.GetMessageAsync();
         }
     }
 }
