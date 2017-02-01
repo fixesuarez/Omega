69422f79a8f035097b450b63e853e9a920376384
DamienGidon <dgidon@intechinfo.fr>
2017-02-01 10:33:47 +0100

    fix insertGroup vuex

--- src/OmegaWebApp/omega_front/src/vuex/actions.js
+++ src/OmegaWebApp/omega_front/src/vuex/actions.js
@@ -191,6 +191,12 @@ export const insertEvent = ({commit}, payload) => {
     commit(types.INSERTEVENT, payload)
 }
 
+export const insertGroup = ({commit}, payload) => {
+    
+    commit(types.INSERTGROUP, payload)
+}
+
+
 export const insertPseudo = ({commit}, payload) => {
     
     commit(types.INSERTPSEUDO, payload)
